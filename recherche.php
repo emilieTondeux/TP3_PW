8c0a052c18bf1aca1bff302cc2ae31a0d574ba44
Emilie TONDEUX <emilie.tondeux@etu.univ-grenoble-alpes.fr>
2020-03-14 17:22:14 +0100

    suite

--- recherche.php
+++ recherche.php
@@ -10,10 +10,16 @@ foreach($tab as $elem) {
     $ID = $elem['id'];
     $JSON2 = tmdbget("movie/".$ID."/credits");
     $output2 = json_decode($JSON2, TRUE);
-    $cast = $output['cast'];
+    $cast = $output2['cast'];
     echo "Id : ".$elem['id']."<br/>";
     echo "Date de sortie : ".$elem['release_date']."<br/>";
     echo "Titre : ".$elem['title']."<br/>";
+	foreach($cast as $elem2) {
+		$ID = $elem2['id'];
+		echo "Name : ".$elem2['name']."<br/>";
+	    echo "Rôle : ".$elem2['character']."<br/>";
+	}
+
     echo "<br/>";
 }
 
