4283751ce5579c55db41af4a9b2fde1396559c07
Emilie TONDEUX <emilie.tondeux@etu.univ-grenoble-alpes.fr>
2020-03-14 17:23:39 +0100

    suite

--- recherche.php
+++ recherche.php
@@ -8,24 +8,6 @@ $ID_Collec = $Tab_Collec["results"][0]["id"];
 $JSON_Search = tmdbget("search/movie", ["query" => "The Lord Of The Rings"]);
 $Tab_Search = json_decode($JSON_Search, TRUE);
 
-<<<<<<< HEAD
-$tab = $output["results"];
-foreach($tab as $elem) {
-    $ID = $elem['id'];
-    $JSON2 = tmdbget("movie/".$ID."/credits");
-    $output2 = json_decode($JSON2, TRUE);
-    $cast = $output2['cast'];
-    echo "Id : ".$elem['id']."<br/>";
-    echo "Date de sortie : ".$elem['release_date']."<br/>";
-    echo "Titre : ".$elem['title']."<br/>";
-	foreach($cast as $elem2) {
-		$ID = $elem2['id'];
-		echo "Name : ".$elem2['name']."<br/>";
-	    echo "Rôle : ".$elem2['character']."<br/>";
-	}
-
-    echo "<br/>";
-=======
 $Tab_Result = $Tab_Search["results"];
 foreach($Tab_Result as $Movies) {
     $ID_Movie = $Movies['id'];
@@ -54,7 +36,6 @@ foreach($Tab_Result as $Movies) {
             echo "<br/>";
         }
     }
->>>>>>> 03e3c53d3c3d53257649976f221bc22daa51669a
 }
 
  ?>
