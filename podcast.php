9878012bae8637b0b48dafb78f893495057d0a39
Emilie TONDEUX <emilie.tondeux@etu.univ-grenoble-alpes.fr>
2020-03-14 19:11:53 +0100

    debut recup titre

--- podcast.php
+++ podcast.php
@@ -2,9 +2,11 @@
 require_once('vendor/dg/rss-php/src/Feed.php');
 $feed = new Feed;
 
-$tab =$feed->load("http://radiofrance-podcast.net/podcast09/rss_14312.xml", NULL, NULL);
+$tab =$feed->loadRss("http://radiofrance-podcast.net/podcast09/rss_14312.xml", NULL, NULL);
 //$tab1 = $feed->toArray($feed->xml);
-var_dump($tab);
-echo $feed->__get();
+//var_dump($tab);
+foreach ($tab->item as $item) {
+	echo $item->title;
+}
 
  ?>
